79860f36de09362db76b717acbae9e2b407ad3eb
kstudilova <kstudilova@gmail.com>
2025-11-27 03:29:49 +0300

    memory allocation and output of results

--- studilova.ksenia/P4/main.cpp
+++ studilova.ksenia/P4/main.cpp
@@ -17,6 +17,10 @@ size_t studilova::string_length(const char* str)
 
 int main()
 {
+    char* input_string = nullptr;
+    char* result1 = nullptr;
+    char* result2 = nullptr;
+
     const size_t max_input_size = 10000;
     char* input_string = new char[max_input_size];
 
@@ -34,4 +38,10 @@ int main()
     }
 
     size_t input_len = studilova::string_length(input_string);
+
+    result1 = new char[input_len + 1];
+    result2 = new char[input_len + 1];
+
+    std::cout << result1 << "\n";
+    std::cout << result2 << "\n";
 }
