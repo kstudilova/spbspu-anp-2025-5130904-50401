fdb0a72308118c6635e97cadfd979e0cb9da6cf4
kstudilova <kstudilova@gmail.com>
2025-11-27 03:06:20 +0300

    checking line entry

--- studilova.ksenia/P4/main.cpp
+++ studilova.ksenia/P4/main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+
+namespace studilova 
+{
+
+}
+
+
 int main()
 {
-    
+    const size_t max_input_size = 10000;
+    char* input_string = new char[max_input_size];
+
+    if (!std::cin.getline(input_string, max_input_size))
+    {
+        std::cerr << "Error: ";
+        delete[] input_string;
+        return 1;
+    }
 }
